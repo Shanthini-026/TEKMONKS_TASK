fd693503ed9de647bfddc48ecfb062a667b0d4db
Shanthini.S <99191683+Shanthini-026@users.noreply.github.com>
2023-09-11 09:09:29 +0530

    scrapp.py

--- scrapp.py
+++ scrapp.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from flask import Flask, jsonify
+from flask import Flask, jsonify,render_template
 
 app = Flask(__name__)
 
@@ -10,8 +10,16 @@ def index():
     soup = BeautifulSoup(response.content, 'html.parser')
     latest_stories_section = soup.find('div', {'class': 'partial latest-stories'})
     stories = latest_stories_section.find_all('a')
-    latest_stories = [{'title': story.text, 'link': story['href']} for story in stories]
+    latest_stories = [{'title': story.text, 'link': "https://time.com"+story['href']} for story in stories]
     return jsonify(latest_stories)
+@app.route('/')
+def content():
+    response = requests.get('https://time.com/')
+    soup = BeautifulSoup(response.content, 'html.parser')
+    latest_stories_section = soup.find('div', {'class': 'partial latest-stories'})
+    stories = latest_stories_section.find_all('a')
+    latest_stories = [{'title': story.text, 'link':"https://time.com"+ story['href']} for story in stories]
+    return render_template('index.html',latest_stories=latest_stories)
 
 if __name__ == '__main__':
     app.run()
